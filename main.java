c57d242f008806ed566ac43ea142b7384612c024
octajul77 <gon14096@uvg.edu.gt>
2015-09-09 18:05:05 -0500

    Update main.java

--- main.java
+++ main.java
@@ -8,13 +8,52 @@
  *
  * @author Admin
  */
-public class main {
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.IOException;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+public class Main{
+	public static void main(String[] args) {
+		try{
+			String linea;
+			BufferedReader doc = new BufferedReader(new FileReader("diccionario.txt"));
+			
+			while((linea = doc.readLine()) != null ){
+				String ingles=" ";
+				String espanol=" ";
+				
+				System.out.println(linea);
+				int contador = 0;
+				while(contador<linea.length()){
+					if(linea.charAt(contador)=='('){
+						while(true){
+							contador=contador+1;
+							if(linea.charAt(contador)==',')
+								break;
+							ingles+=linea.charAt(contador);
+						}
+					}
+					if(linea.charAt(contador)==' ')
+						while(true){
+							contador=contador+1;
+							if(linea.charAt(contador)==')')
+								break;
+							espanol+=linea.charAt(contador);
+						}
+					contador=contador+1;
+				}
+				
+				Association association = new Association(ingles,espanol);
+				
+			}
+			doc.close();
+		}
+		catch(IOException io){
+			System.out.println("Error, ocurrio un problema en lectura del documento u operacion");
+		}
+		catch(FileNotFoundException file){
+			System.out.println("Error, el archivo no se encuentra");
+		}
+		
+	}
