470b68c397b0edfd4eaa94cf179bcc8c95ff58b7
octajul77 <gon14096@uvg.edu.gt>
2015-09-09 19:36:05 -0500

    Update main.java

--- main.java
+++ main.java
@@ -53,5 +53,36 @@ public class main{
 			System.out.println("Error, ocurrio un problema en lectura del documento u operacion");
 	
 		}
+		
+		
+	try{
+			String linea;
+			String texto=" ";
+			BufferedReader doc2 = new BufferedReader(new FileReader("texto.txt"));
+			System.out.print("Original: ");
+			while((linea = doc2.readLine()) != null ){
+				int contador2=0;
+				String palabra="";
+				System.out.println(linea);
+				while(contador2<linea.length()){
+					if(linea.charAt(contador2)!=' ' & linea.charAt(contador2)!='.')
+						palabra+=linea.charAt(contador2);
+					else{
+						palabra = '*'+palabra+"*";
+						
+						
+						palabra+=linea.charAt(contador2);
+						texto+=palabra;
+						palabra="";
+					}
+					contador2++;
+				}
+			}
+			doc2.close();
+			System.out.println("Traduccion: "+texto);
+	}
+		catch(IOException io){
+			System.out.println("Error, ocurrio un problema en lectura del documento u operacion");
+		}
 	}
 }
